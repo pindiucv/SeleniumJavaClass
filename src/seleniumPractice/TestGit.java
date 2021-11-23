1b4efd6bd4ddcb3bce45a868d4fa36b0c4c04d29
Veruncic <veronica.pindiuc@gmail.com>
2021-11-23 13:45:34 -0600

    hello

--- src/seleniumPractice/TestGit.java
+++ src/seleniumPractice/TestGit.java
@@ -3,7 +3,6 @@ package seleniumPractice;
 public class TestGit {
 
     public static void main(String[] args) {
-        System.out.println("New code to Git");
+        System.out.println("hello");
     }
-
 }
