68ed5f469a293c57822fbac267fb014ac6081741
Veruncic <veronica.pindiuc@gmail.com>
2021-11-23 11:23:24 -0600

    Initial commit

---
+++ src/seleniumPractice/MyFirstSeleniumTest.java
@@ -0,0 +1,22 @@
+package seleniumPractice;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class MyFirstSeleniumTest {
+
+    public static void main(String[] args) {
+
+      System.setProperty("webdriver.chrome.driver", "C:\\Users\\Veruncic\\JavaPractice\\drivers\\chromedriver.exe");
+
+       ChromeDriver driver = new ChromeDriver();
+
+    }
+
+
+
+
+
+}
